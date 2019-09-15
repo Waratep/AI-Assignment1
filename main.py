f654637d058dbd2e875b573922d96c95c290311a
Tubboon <tubboon.wara@icloud.com>
2019-09-15 23:35:20 +0700

    finished !

--- main.py
+++ main.py
@@ -1,63 +1,61 @@
-from Map import getMap , getEmtryMap
-from Tree import Node , addData , printLevelsRecursively ,height ,printSingleLevelRecursively
-from Maze import checkcanwego 
-
+from Map import getMap
 import time
+
+def checkwall(map,current):
+    x = current[0]
+    y = current[1]
+    if(map[x][y] != 1):
+        return True
+    return False
+
+def dfs_paths(graph, start, goal):
+    
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+                
+            else:
+                stack.append((next, path + [next]))
 
 if __name__ == "__main__":
 
-    x = 10
-    y = 10
-
-    lastcurrent = ()
-    current = (x,y)
-    root = Node(current)
-    level = 1
-
     map = getMap()
 
-    propofmap = 4
-    loop = 0
-    while (loop < propofmap):
+_buffer = []
+next_current = []
+current = (10,10)
+graph = {}
+graph[current] = set()
 
-        if (1,1) in current:
-            break
+next_current.append(current)
+limit = 20
+level = 0
+start = (10,10)
+end = (1,1)
 
-        levellist = []
-        printSingleLevelRecursively(root,level,levellist)
+seconds = time.time()
 
-        current = levellist
+while(level < limit):
 
-        print('current 1',current)
-        
-        printLevelsRecursively(root)
+    if list(dfs_paths(graph, start, end)) != [] : 
+        print('fined path from start to end')
+        print(list(dfs_paths(graph, start, end)))
+        print('used time',time.time() - seconds , 'seconds') 
+        break
+    for current in next_current:
+        x = current[0]
+        y = current[1]
+        array = [(x,y-1),(x,y+1),(x-1,y),(x+1,y)]
+        for i in range (len(array)):            
+            if checkwall(map,array[i]):
+                if array[i] not in graph:
+                    graph[array[i]] = set()
+                graph[current].add(array[i])
+                _buffer.append(array[i])
 
-        for x in current:
-            howmany_canwego  = checkcanwego( x , map )
-            print('howmany_canwego',howmany_canwego)
-            for cordinate in howmany_canwego:
-                if cordinate != None:
-                    addData(root,cordinate)
-                    print('add',cordinate)
-            current = levellist
-            print('current',x)
-        level += 1
-
-
-        printLevelsRecursively
-        levellist = []
-        printSingleLevelRecursively(root,level,levellist)
-        print('level',level)
-        print('levellist',levellist)
-        current = levellist
-        print()
-        print('current 2',current)
-        print('============================================================')
-
-        print("#######################################################")
-        printLevelsRecursively(root)
-        print("#######################################################")
-
-        loop += 1
-
-
+    next_current = _buffer
+    _buffer = []
+    level += 1
